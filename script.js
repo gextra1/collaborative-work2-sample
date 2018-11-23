707c69faefd96330e4cb3256a338ab1722612761
Georgy Extra <georgyextra@gmail.com>
2018-11-23 03:11:46 -0500

    adding starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var linearGradient = document.querySelector("h3");
+
+function setBackground() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value + ", " 
+    + color2.value + ")";
+    linearGradient.textContent = body.style.background;
+}
+
+color1.addEventListener("input", setBackground);
+
+color2.addEventListener("input", setBackground);
